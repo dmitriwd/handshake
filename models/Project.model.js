645f3b19e828e66b010c57ff23cab1e2abdc10a2
GosiaMas <malgorzata.maslowska81@gmail.com>
2020-11-24 12:43:11 +0000

    background color

--- models/Project.model.js
+++ models/Project.model.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-const mongoose = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const projectSchema = new Schema({
