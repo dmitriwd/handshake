91349ca6cea2ff2139c7200b986d225ea4ae0d0e
GosiaMas <malgorzata.maslowska81@gmail.com>
2020-11-24 09:27:04 +0000

    with logout

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,11 +1,14 @@
 module.exports = (req, res, next) => {
   // checks if the user is logged in when trying to access a specific page
-  if (
-    (!req.session.freelancer && req.session.company) ||
-    (req.session.freelancer && !req.session.company)
-  ) {
+  if (!req.session.freelancer && !req.session.company) {
     return res.redirect("/login");
   }
+  // if (
+  //   (!req.session.freelancer && req.session.company) ||
+  //   (req.session.freelancer && !req.session.company)
+  // ) {
+  //   return res.redirect("/login");
+  // }
 
   next();
 };
